30220f57bc5052cb05ed5c7e3dc01c763152d175
mattlangford <matthew.langford95@gmail.com>
2016-10-02 00:19:18 -0400

    Add setup for python installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lqrrt',
+      version='1.0',
+      description='Kinodynamic RRT Implementation',
+      author='Jason Nezvadovitz',
+      packages=['lqrrt'],
+     )
