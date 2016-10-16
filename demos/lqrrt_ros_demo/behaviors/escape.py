4b3ef1e9208e9c01a26cca33601480ae89ebfb21
jnez71 <nez.jason7@gmail.com>
2016-10-16 12:34:06 -0400

    ESCAPE SS: explore about seed

--- demos/lqrrt_ros_demo/behaviors/escape.py
+++ demos/lqrrt_ros_demo/behaviors/escape.py
@@ -11,9 +11,6 @@ import lqrrt
 
 ################################################# DYNAMICS
 
-magic_rudder = 6000
-look_at = None
-
 def dynamics(x, u, dt):
 	"""
 	Returns next state given last state x, wrench u, and timestep dt.
@@ -32,16 +29,6 @@ def dynamics(x, u, dt):
 		if v >= 0:
 			D[i] = D_pos[i]
 
-	# Heading controller for staring at some look_at point
-	if look_at is not None:
-		vec = look_at - x[:2]
-		ang = np.arctan2(vec[1], vec[0])
-		c = np.cos(x[2])
-		s = np.sin(x[2])
-		cg = np.cos(ang)
-		sg = np.sin(ang)
-		u[2] = magic_rudder*np.arctan2(sg*c - cg*s, cg*c + sg*s)
-
 	# Actuator saturation
 	u = B.dot(np.clip(invB.dot(u), -thrust_max, thrust_max))
 
@@ -55,8 +42,8 @@ def dynamics(x, u, dt):
 
 ################################################# POLICY
 
-kp = np.diag([150, 150, 400])
-kd = np.diag([150, 150, 100])
+kp = np.diag([150, 150, 1000])
+kd = np.diag([150, 150, 1])
 S = np.diag([1, 1, 1, 1, 1, 1])
 
 def lqr(x, u):
@@ -77,13 +64,15 @@ def lqr(x, u):
 goal_buffer = [free_radius, free_radius, np.inf, np.inf, np.inf, np.inf]
 error_tol = np.copy(goal_buffer)
 
+sf = 3
+
 def gen_ss(seed, goal):
 	"""
 	Returns a sample space given a seed state and goal state.
 
 	"""
-	return [(min([seed[0], goal[0]]) - ss_buff, max([seed[0], goal[0]]) + ss_buff),
-			(min([seed[1], goal[1]]) - ss_buff, max([seed[1], goal[1]]) + ss_buff),
+	return [(seed[0] - sf*ss_buff, seed[0] + sf*ss_buff),
+			(seed[1] - sf*ss_buff, seed[1] + sf*ss_buff),
 			(-np.pi, np.pi),
 			(-abs(velmax_neg_plan[0]), velmax_pos_plan[0]),
 			(-abs(velmax_neg_plan[1]), velmax_pos_plan[1]),
@@ -97,5 +86,5 @@ constraints = lqrrt.Constraints(nstates=nstates, ncontrols=ncontrols,
 planner = lqrrt.Planner(dynamics, lqr, constraints,
 						horizon=horizon, dt=dt, FPR=FPR,
 						error_tol=error_tol, erf=unset,
-						min_time=basic_duration, max_time=basic_duration, max_nodes=max_nodes,
+						min_time=0, max_time=5, max_nodes=max_nodes,
 						sys_time=unset, printing=False)
